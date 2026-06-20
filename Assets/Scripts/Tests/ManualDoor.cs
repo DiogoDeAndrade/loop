0625fc6c10ae5d650dc97138b1a971a00a4bff0d
Diogo de Andrade <diogo.g.andrade@gmail.com>
2026-06-20 18:39:02 +0100

    Test nav mesh obstacle

--- Assets/Scripts/Tests/ManualDoor.cs
+++ Assets/Scripts/Tests/ManualDoor.cs
@@ -5,8 +5,20 @@ public class ManualDoor : MonoBehaviour, INavMeshLinkCondition
 {
     [SerializeField] private bool isOpen;
 
+    Collider2D _collider;
+
     public bool NavCanPass(NavMeshAgent2d agent, NavMeshLink2d link)
     {
         return isOpen;
+    }
+
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        _collider.enabled = !isOpen;
     }
 }
